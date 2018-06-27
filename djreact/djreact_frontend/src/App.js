ea5bf0e03edef09447f2ba951607ec4d85c38e69
Yameogo <jyameogo@deloitte.ca>
2018-06-26 20:10:54 -0400

    basic react app + bootstrap + reactstrap

--- djreact/djreact_frontend/src/App.js
+++ djreact/djreact_frontend/src/App.js
@@ -1,19 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import { Button, Container, Row, Col } from "reactstrap";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [],
+      cur_id: 0,
+      is_creating: true
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <React.Fragment>
+        <Container>
+          <Row>
+            <Col xs="10">
+              <h2>Realtime notes</h2>
+            </Col>
+            <Col xs="2">
+              <Button color="primary">Create a new note</Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs="4">
+              <h5>List notes here....</h5>
+            </Col>
+            <Col xs="8">
+              <p>Content editing here...</p>
+            </Col>
+          </Row>
+        </Container>
+      </React.Fragment>
     );
   }
 }
