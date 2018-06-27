59bae545a1f5897f8cb8d9175d86328533a8ad43
Yameogo <jyameogo@deloitte.ca>
2018-06-26 20:51:48 -0400

    FE: handle click event

--- djreact/djreact_frontend/src/App.js
+++ djreact/djreact_frontend/src/App.js
@@ -2,16 +2,52 @@ import React, { Component } from "react";
 
 import { Button, Container, Row, Col } from "reactstrap";
 
+import ListNotes from "./components/ListNotes";
+
+var static_notes = [
+  {
+    id: 1,
+    title: "firstREACT",
+    content: "This is the first REACT note"
+  },
+  {
+    id: 2,
+    title: "secondREACT",
+    content: "This is the second REACT note"
+  },
+  {
+    id: 3,
+    title: "thirdREACT",
+    content: "This is the third REACT note"
+  }
+];
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      notes: [],
-      cur_id: 0,
+      notes: static_notes,
+      current_id: 0,
       is_creating: true
     };
+
+    this.handleClickNoteEvent = this.handleClickNoteEvent.bind(this);
+    this.handleAddNoteEvent = this.handleAddNoteEvent.bind(this);
   }
+
+  handleClickNoteEvent(id) {
+    this.setState(prevState => {
+      return { is_creating: false, current_id: id };
+    });
+  }
+
+  handleAddNoteEvent() {
+    this.setState(prevState => {
+      return { is_creating: true };
+    });
+  }
+
   render() {
     return (
       <React.Fragment>
@@ -21,15 +57,23 @@ class App extends Component {
               <h2>Realtime notes</h2>
             </Col>
             <Col xs="2">
-              <Button color="primary">Create a new note</Button>
+              <Button color="primary" onClick={this.handleAddNoteEvent}>
+                Create a new note
+              </Button>
             </Col>
           </Row>
           <Row>
             <Col xs="4">
-              <h5>List notes here....</h5>
+              <ListNotes
+                notes={this.state.notes}
+                eventHandler={id => this.handleClickNoteEvent(id)}
+              />
             </Col>
             <Col xs="8">
               <p>Content editing here...</p>
+              {this.state.is_creating
+                ? "Creating now..."
+                : `Editing note with id ${this.state.current_id}`}
             </Col>
           </Row>
         </Container>
--- djreact/djreact_frontend/src/components/ListNotes.js
+++ djreact/djreact_frontend/src/components/ListNotes.js
@@ -1,1 +1,21 @@
 import React from "react";
+
+import { ListGroup, ListGroupItem } from "reactstrap";
+
+const ListNotes = ({ notes, eventHandler }) => {
+  let notes_list = notes.map(note => {
+    return (
+      <ListGroupItem
+        key={note.id}
+        href="#"
+        onClick={id => eventHandler(note.id)}
+      >
+        <p>{note.title}</p>
+      </ListGroupItem>
+    );
+  });
+
+  return <ListGroup>{notes_list}</ListGroup>;
+};
+
+export default ListNotes;
